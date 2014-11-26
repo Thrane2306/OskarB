79cadcd249547f65bdcec14ec1df4afc663e74e9
oskarbonde <sisu_bonde@hotmail.com>
2014-11-26 13:38:25 +0100

    LOGIN m Logo

--- src/gui/CalendarLogin.java
+++ src/gui/CalendarLogin.java
@@ -11,27 +11,27 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.JPasswordField;
 
 public class CalendarLogin extends JPanel{
 	private JTextField textField;
-	private JTextField textField_1;
+	private JPasswordField passwordField;
 	public CalendarLogin() {
 		setBounds(new Rectangle(0, 0, 1180, 900));
 		setLayout(null);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.text);
 		panel.setBounds(12, 13, 1168, 887);
 		add(panel);
 		panel.setLayout(null);
-		
-		JLabel lblLogin = new JLabel("LOGIN");
-		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		lblLogin.setBounds(528, 88, 150, 72);
-		panel.add(lblLogin);
-		
+				
 		JLabel lblUsername = new JLabel("Username:");
 		lblUsername.setBounds(528, 224, 150, 16);
 		panel.add(lblUsername);
@@ -45,11 +45,6 @@ public class CalendarLogin extends JPanel{
 		lblPassword.setBounds(528, 294, 150, 16);
 		panel.add(lblPassword);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(528, 331, 150, 22);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
 		JButton btnOk = new JButton("OK");
 		btnOk.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -59,10 +54,14 @@ public class CalendarLogin extends JPanel{
 		btnOk.setBounds(550, 379, 97, 25);
 		panel.add(btnOk);
 		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(528, 323, 150, 22);
+		panel.add(passwordField);
 		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(CalendarLogin.class.getResource("/gui/CBS logo.png")));
+		lblNewLabel.setBounds(324, 27, 646, 198);
+		panel.add(lblNewLabel);
 		
-		
-	
-		
-		}
 	}
+}
--- src/gui/CalendarPanel.java
+++ src/gui/CalendarPanel.java
@@ -4,9 +4,11 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 import java.awt.Rectangle;
+import javax.swing.JButton;
 
 public class CalendarPanel extends JPanel{
 	public static JPanel panelCal, panelOpt;
+	private JButton btnSettings;
 	public CalendarPanel() {
 		setBounds(new Rectangle(0, 0, 1180, 900));
 		setLayout(null);
@@ -22,6 +24,11 @@ public class CalendarPanel extends JPanel{
 		panelOpt.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panelOpt.setBounds(10, 212, 294, 677);
 		add(panelOpt);
+		panelOpt.setLayout(null);
+		
+		btnSettings = new JButton("Settings");
+		btnSettings.setBounds(12, 13, 270, 50);
+		panelOpt.add(btnSettings);
 		panelCal.setLayout(null);
 		CalendarTest cT = new CalendarTest();
 		cT.setBounds(10, 11, 836, 659);
